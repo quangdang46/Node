f140eb217b94463025da7490f5715ab1adb99c76
quangdang46 <tranquangdang21@gmail.com>
2023-10-30 19:42:30 +0700

    error check pass

--- TH/Lab 6/routes/index.js
+++ TH/Lab 6/routes/index.js
@@ -19,24 +19,26 @@ router.get("/login", function (req, res, next) {
 router.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  console.log(username, password);
   try {
     const sql = "SELECT * FROM users WHERE username = ?";
     connection.query(sql, [username], function (err, result) {
       if (err) throw err;
       if (result.length > 0) {
-        bcrypt.compare(password, result[0].password, function (err, response) {
-          if (response) {
-            req.session.loggedin = true;
-            req.session.username = username;
-            res.redirect("/");
-          } else {
-            // res.send("Incorrect Password!");
-            req.session.loggedin = true;
-            req.session.username = username;
-            res.redirect("/");
-          }
-        });
+        const response = bcrypt.compareSync(password, result[0].password);
+        if (response) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect("/");
+        } else {
+          // res.send("Incorrect Password!");
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect("/");
+        }
+
+        // bcrypt.compare(password,result[0].password,function(err,response){
+        //   console.log(response);
+        // })
       } else {
         res.send("Username does not exist!");
       }
@@ -55,27 +57,26 @@ router.post("/register", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
-
   try {
     // validate password
     if (password !== repassword) {
       res.send("Passwords do not match!");
     }
 
-    const sql = "SELECT * FROM users WHERE username = ?";
-    const result = await connection.query(sql, [username]);
-
-    if (result.length > 0) {
-      res.send("That username is already in use!");
-    } else {
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-
-      const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-      await connection.query(sql, [username, passwordHash]);
-
-      res.redirect("/login");
-    }
+    let sql = "SELECT * FROM users WHERE username = ?";
+    connection.query(sql, [username], function (error, results) {
+      if (error) throw error;
+      if (results.length > 0) {
+        res.send("That username is already in use!");
+      } else {
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(password, salt);
+        console.log(passwordHash);
+        const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        connection.query(sql, [username, passwordHash]);
+        res.redirect("/login");
+      }
+    });
   } catch (err) {
     console.error(err);
     res.send(err.message);
